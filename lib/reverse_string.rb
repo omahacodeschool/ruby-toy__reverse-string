55bde84eea55091c13ada56840f02cbd7060d107
bethanyhaubert <bethany.haubert@gmail.com>
2016-01-11 23:40:16 +0000

    first method unsuccessful.

--- lib/reverse_string.rb
+++ lib/reverse_string.rb
@@ -1,5 +1,13 @@
 def reverse_characters(str)
+    array = [str.split("")]
+    reverse_array = []
+    loop.each do
+        reverse_array[array[-1, 1]]
+    end
+  return reverse_array
 end
 
+
 def reverse_words(str)
+    
 end
