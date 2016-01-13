36d8924e85f0fab947d5588a8923cbad4c68eccd
Derek Pressnall <derekpressnall@yahoo.com>
2016-01-13 02:51:26 +0000

    bad code

--- lib/reverse_string.rb
+++ lib/reverse_string.rb
@@ -1,5 +1,17 @@
 def reverse_characters(str)
+  str = str.chars
+  new_str = ''
+  until new_str.length == str.length
+  new_str << str[-1] 
+  str.pop
+  end
+  return new_str 
 end
+ 
+
 
 def reverse_words(str)
+  
 end
+    
+
