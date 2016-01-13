275359036e9c79be353502e4ad2169bf4604cd68
Melinda <melinda.deinert@gmail.com>
2016-01-13 05:14:41 +0000

    Done

--- lib/reverse_string.rb
+++ lib/reverse_string.rb
@@ -1,5 +1,37 @@
 def reverse_characters(str)
+    str = str.split(//);
+    str2 = []
+    str2[0] = str[-1]
+    str2[1] = str[-2]
+    str2[2] = str[-3]
+    str2[3] = str[-4]
+    str2[4] = str[-5]
+    str2[5] = str[-6]
+    str2[6] = str[-7]
+    str2[7] = str[-8]
+    str2[8] = str[-9]
+    str2[9] = str[-10]
+    str2[10] = str[-11]
+    str2[11] = str[-12]
+    str2[12] = str[-13]
+    str2[13] = str[-14]
+    str2[14] = str[-15]
+    str2[15] = str[-16]
+    str2[16] = str[-17]
+    str2[17] = str[-18]
+    str2[18] = str[-19]
+    return str2.join("")
 end
 
+
+
 def reverse_words(str)
+    str = str.split(/ /);
+    str2 = []
+    str2[0] = str[-1]
+    str2[1] = str[-2]
+    str2[2] = str[-3]
+    str2[3] = str[-4]
+    str2[4] = str[-5]
+    return str2.join(" ")
 end
