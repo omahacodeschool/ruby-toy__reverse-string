a058b59f9591b07582090d46ec803199a1a67949
Courtney Burns <courtneylynnburns@gmail.com>
2016-01-13 23:44:07 +0000

    fin

--- lib/reverse_string.rb
+++ lib/reverse_string.rb
@@ -1,12 +1,11 @@
 def reverse_characters(str)
 #Ok this is a little better but im still not sure how to use the .join to get it back to a string
 #The array can be called back backwards but its not "saved" anywhere in that order so it cant be converted to a string
-string='Today is a good day'
-array=string.split("")
-x=array.length
-array.length.times do
-    x -= 1
-    puts array[x]
+string="Today is a good day"
+array=string.split
+x=0
+for loop in array
+puts array[x-=1]
 end
 
 def reverse_words(str)
