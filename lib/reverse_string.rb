db60461174b0eec73eac26d5fa7f483afd2e554f
Derek Pressnall <derekpressnall@yahoo.com>
2016-01-14 17:18:09 +0000

    Done

--- lib/reverse_string.rb
+++ lib/reverse_string.rb
@@ -1,17 +1,24 @@
 def reverse_characters(str)
   reverse_characters = str.chars
   new_str = ''
-  until new_str.length == str.length
-  new_str << reverse_characters[-1]
-  reverse_characters.pop 
-  end
+until new_str.length == str.length
+    new_str << reverse_characters[-1]
+    reverse_characters.pop 
+end
   return new_str
 end
  
 
 
 def reverse_words(str)
-  
+  reverse_words = str.split
+  new_str = ''
+while new_str.length <= str.length do
+    new_str << reverse_words.pop << ' '
 end
+return new_str.chomp(' ')
+end
+  
+
     
 
