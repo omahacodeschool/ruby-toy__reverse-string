3cd32e7c2517314d5956485286de9a63da546590
bethanyhaubert <bethany.haubert@gmail.com>
2016-01-15 01:36:00 +0000

    need to figure out how to split string

--- lib/reverse_string.rb
+++ lib/reverse_string.rb
@@ -3,14 +3,22 @@ def reverse_characters(str)
     array = str.split("")
     #create a holder for to-be-reversed characters
     reverse_array = []
-    #going through each character in the array, store it in the last position of the new holder array
+    #going through each character in the array, store each one in the first position of the new holder array
     array.each do |n|
-        reverse_array.insert[-1,n]
+        reverse_array.insert(1,n)
     end
-  return reverse_array
+  return reverse_array.join
 end
 
 
 def reverse_words(str)
-    
+    #create array dividing up string into individual word strings, but defining a word as a group of characters divided by a space (or with nil in front of or after due to it being at the beginning or end of string)
+    array = str.split(figure out how to do above thing)
+    #create holder for to-be-reversed words
+    reverse_arr = []
+    #insert each word-string in the first position of the new holder array
+    array.each do |n|
+        reverse_arr.insert(1,n)
+    end
+    return reverse_arr.join
 end
