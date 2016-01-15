261eab6bd94cb466dca51b01d059cc303d55cd2b
Jillian <jwaggoner1016@gmail.com>
2016-01-15 21:23:12 +0000

    Reverse

--- lib/reverse_string.rb
+++ lib/reverse_string.rb
@@ -1,6 +1,10 @@
-def reverse_characters(str)
-  Hi Sumeet -- It's taking me FORevER to come up with these solutions so I just wanted to run my ideas past you to make sure I'm on the right track here. I think for both of the reverse (without using .reverse) problems, what needs to happen is a count of all of the characters in the string (or in the 2nd problem, converting to an array and counting the elements) using .length, then a one-at-a-time recall of each character/element backwards from the highest number. Is that sensible or am I barking up the wrong tree? If I'm correct, I don't need any specific help right now, I guess, but I may come back for additional assistance with the def/end coding. I feel like this is the first (or almost the first?) time I'm seeing "def..." and I may get stuck. 
+def backwards(a)
+ a = a.length - 1
+ while a >= 0
+ return a
 end
 
-def reverse_words(str)
-end
+puts ( backwards("Hello world.") )
+
+Hi Sumeet -- Thank you for being flexible on the deadline here. I hope that the remaining exercises will not take me quite so long to understand. Anyhow, I've got some broken code here. For the "create a loop to start at .length-1 and return each character, one at a time, in descending numerical order" step, I can't figure how I'd break it down any further than saying that while the user input is >=0, print that input. I think (?) the .length-1 establishes where to start and to move backwards one at a time, doesn't it? 
+ 
