8626cda79affd5677d696dd1468e27763c445c86
bethanyhaubert <bethany.haubert@gmail.com>
2016-01-15 00:06:16 +0000

    added thought process

--- lib/reverse_string.rb
+++ lib/reverse_string.rb
@@ -1,8 +1,11 @@
 def reverse_characters(str)
+    #create an array dividing up the string into individual characters
     array = [str.split("")]
+    #create a holder for to-be-reversed characters
     reverse_array = []
+    #going through each character in the array, store it in the last position of the new holder array
     loop.each do
-        reverse_array[array[-1, 1]]
+        reverse_array[array[-1,1]]
     end
   return reverse_array
 end
