a021115015939f09378120e3364cebd7d9742d94
Patrick Clark <patclark1958@gmail.com>
2016-01-16 05:33:04 +0000

    Done and dusted

--- lib/reverse_string.rb
+++ lib/reverse_string.rb
@@ -1,5 +1,14 @@
 def reverse_characters(str)
+startarray=str.split("")
+finalarray = []
+finalarray.push(startarray.pop) until startarray.empty? 
+return finalarray.join
 end
 
 def reverse_words(str)
+startarray=str.split(" ")
+finalarray = []
+finalarray.push(startarray.pop) until startarray.empty? 
+return finalarray.join(" ")
 end
+
