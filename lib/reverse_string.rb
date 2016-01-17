560e9ccdc49e4e9f8fb35f0732eff5485c0c4eb0
Melinda <melinda.deinert@gmail.com>
2016-01-17 22:05:49 +0000

    Done

--- lib/reverse_string.rb
+++ lib/reverse_string.rb
@@ -1,13 +1,21 @@
 def reverse_characters(str)
     str = str.split(//)
-    x = str.length
-    x.times {x = x - 1; print str[x]}
-    return 
+    str2 = []
+    str.length.times do
+        x = str[-1]
+        str2.push(x)
+        str.pop
+    end
+    return str2.join("")
 end
 
 def reverse_words(str)
     str = str.split(/ /)
-    x = str.length
-    x.times {x = x - 1; print str[x]}
-    return 
+    str2 = []
+    str.length.times do
+        x = str[-1]
+        str2.push(x)
+        str.pop
+    end
+    return str2.join(" ")
 end
