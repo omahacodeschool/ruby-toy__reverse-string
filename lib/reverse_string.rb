c2b541bd49750ac0f13874f2e5cfb4fcd38b6f09
Jillian <jwaggoner1016@gmail.com>
2016-01-18 01:25:35 +0000

    NOT done

--- lib/reverse_string.rb
+++ lib/reverse_string.rb
@@ -5,6 +5,3 @@ def backwards(a)
 end
 
 puts ( backwards("Hello world.") )
-
-Hi Sumeet -- Thank you for being flexible on the deadline here. I hope that the remaining exercises will not take me quite so long to understand. Anyhow, I've got some broken code here. For the "create a loop to start at .length-1 and return each character, one at a time, in descending numerical order" step, I can't figure how I'd break it down any further than saying that while the user input is >=0, print that input. I think (?) the .length-1 establishes where to start and to move backwards one at a time, doesn't it? 
- 
