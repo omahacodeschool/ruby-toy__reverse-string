1e9de685ca988eeb617ceb135c267a2b186a1728
Michael Meier <mmeier61@yahoo.com>
2016-01-18 17:11:31 +0000

    One Done

--- lib/reverse_string.rb
+++ lib/reverse_string.rb
@@ -1,4 +1,12 @@
 def reverse_characters(str)
+puts 'Please give me a sentance to mirror'
+string = gets.chomp
+stingray = string.split("")
+mirror = []
+until stingray.empty?
+    mirror << stingray.pop
+end
+mirror.join
 end
 
 def reverse_words(str)
