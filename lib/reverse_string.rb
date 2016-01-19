63dfec03aa7f00103b485b465fd805bc9f67b3c8
amymariewall <amymariewall@gmail.com>
2016-01-19 19:01:44 +0000

    Exercise Reverse Response

--- lib/reverse_string.rb
+++ lib/reverse_string.rb
@@ -1,5 +1,17 @@
 def reverse_characters(str)
+  phrase = ""
+  letters = str.split""
+  letters.size.times{phrase << letters.pop}
+  return phrase
 end
 
+reverse_characters("Today is a good day")
+
 def reverse_words(str)
+  reverse_phrase_array = []
+  phrase = str.split" "
+  phrase.size.times{reverse_phrase_array << phrase.pop}
+  return reverse_phrase_array.join" "
 end
+
+reverse_words("Today is a good day")
