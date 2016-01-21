f7394dcc56d89e19764a609328b58259b599e4c1
Jeremy Stoll <j8r8my@gmail.com>
2016-01-21 02:16:38 +0000

    Done

--- lib/reverse_string.rb
+++ lib/reverse_string.rb
@@ -1,5 +1,26 @@
 def reverse_characters(str)
+    array1 = []
+    array2 = []
+    
+    str.split(//).each do |c|
+        array1 << c
+    end
+    
+    until array1.empty?
+      array2 << array1.pop
+    end
+  return array2.join    
 end
 
 def reverse_words(str)
+    array1 = []
+    array2 = []
+    str.split(" ").each do |c|
+        array1 << c
+    end
+    
+    until array1.empty?
+      array2 << array1.pop
+    end
+  return array2.join(" ")    
 end
