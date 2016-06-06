de2be9b637199a7eb039ea8db98a1b141f7d3e28
Cory Johnson <cory.r.johnson@cox.net>
2016-06-06 19:03:39 +0000

    Cory Johnson ruby string reverse

--- lib/reverse_string.rb
+++ lib/reverse_string.rb
@@ -1,5 +1,30 @@
 def reverse_characters(str)
+
+    i = 0    
+    n = str.size - 1
+    ansstr = " "*n
+    
+while(i<str.size)
+    ansstr[i] = str[n-i]
+    i = i + 1
+end
+
+return ansstr
 end
 
 def reverse_words(str)
+    
+    strarr = str.split(" ")
+    ansarrsize = strarr.size
+    i = 0
+    ansarr = Array.new []
+    
+while(i<ansarrsize)
+    ansarr[i] = strarr[ansarrsize - i - 1]
+    i = i + 1
+end
+
+    ansstr = ansarr.join(" ")
+    return ansstr
+    
 end
