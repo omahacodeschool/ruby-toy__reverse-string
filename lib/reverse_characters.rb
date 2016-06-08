47226e49c78bddc68d7e2aa9232647be69ce93b7
Jerry Samuels Jr <jsamuelsjr@gmail.com>
2016-06-08 19:52:44 +0000

    Submitting my Reverse String portion of the advanced exercises.

--- lib/reverse_characters.rb
+++ lib/reverse_characters.rb
@@ -1,2 +1,21 @@
+#To test my code in repl.it, created an str variable using the line below:
+#str = "Today is a good day".split("")
+
 def reverse_characters(str)
+arr_end = str.length - 1
+new_arr = Array.new
+
+(arr_end).downto(0) do |i|
+	#Create a variable to hold the array element's value for each iteration 
+	z = str[i]
+	#Add each iteration's value to the end of the array
+	new_arr.push(z)
+end
+
+#Creates the final reversed string
+fnl_arr = new_arr.join
+
+#Print value of final variable
+puts fnl_arr
+    
 end
