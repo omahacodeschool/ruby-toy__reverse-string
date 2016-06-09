652aa61d39a934044408db3c094fbf20e597cd57
John <CodeMorrow@yahoo.com>
2016-06-09 16:20:35 +0000

    Reverse Strings

--- lib/reverse_string.rb
+++ lib/reverse_string.rb
@@ -1,5 +1,17 @@
 def reverse_characters(str)
+    reverse_characters_array = []
+        string_array = str.split("")
+        string_array.each do |c|
+            reverse_characters_array.unshift(c)
+        end
+        return reverse_characters_array.join
 end
 
 def reverse_words(str)
+    reverse_words_array = []
+        string_array = str.split(" ")
+        string_array.each do |w|
+            reverse_words_array.unshift(w)
+        end
+        return reverse_words_array.join(" ")
 end
