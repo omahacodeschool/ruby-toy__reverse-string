194ef712993518117f975e7e8001e3c5a3ffda56
Michelle Cali <mctexx@gmail.com>
2016-06-09 18:58:50 +0000

    Submitting code

--- lib/reverse_characters.rb
+++ lib/reverse_characters.rb
@@ -1,2 +1,8 @@
 def reverse_characters(str)
+  word = str.split("")
+  newstr = ""
+  (word.length - 1).downto(0) do |i|
+    newstr = newstr + word[i]
+  end
+  return newstr
 end
--- lib/reverse_words.rb
+++ lib/reverse_words.rb
@@ -1,2 +1,8 @@
 def reverse_words(str)
+  sentence = str.split(" ")
+  new_sentence = []
+  (sentence.length - 1).downto(0) do |i|
+    new_sentence << sentence[i]
+  end
+  return new_sentence.join(" ")
 end
