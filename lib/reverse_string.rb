5094dc359cd7804af51eb9fb52e4836f8a794ab9
Alex <noto.alex.j@gmail.com>
2016-06-10 04:30:35 +0000

    Added thought process comments

--- lib/reverse_string.rb
+++ lib/reverse_string.rb
@@ -15,3 +15,7 @@ def reverse_words(str)
 		end
 	return newarr.join(" ")
 end
+#For these exercises, I tried to use simple methods that I already knew. If a string is reversed, the last letter/word of the
+#original string becomes the first letter/word of the reversed string, and so on. Arrays are easier to manipulate, so I began 
+#by converting the inputted strings to arrays. I then wrote a function that repeatedly removes the last letter/word of the original
+#array and then pushes it to the end of a new array. After the function finishes, the resulting array is a reversed copy of the original.
