7337d2b739d47872b3c1f61fce2df3facbe3ee64
Matt Hughes <mhughes27@gmail.com>
2016-06-11 16:40:28 +0000

    Submitting Reverse the Srting WIP

--- lib/reverse_characters.rb
+++ lib/reverse_characters.rb
@@ -1,2 +1,9 @@
 def reverse_characters(str)
+    
+#Algorithmic Process
+#Create new_string
+#Take the last character from str and add to new_string
+#Continue the process: take the next to last character in str and add to the end of new_string
+
+
 end
