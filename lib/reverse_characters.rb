69a0cc955c403faff4ca15eaf8c6bca63817d4b1
Matt Hughes <mhughes27@gmail.com>
2016-06-13 22:24:54 +0000

    Submitting Ruby Reverse String

--- lib/reverse_characters.rb
+++ lib/reverse_characters.rb
@@ -5,5 +5,13 @@ def reverse_characters(str)
 #Take the last character from str and add to new_string
 #Continue the process: take the next to last character in str and add to the end of new_string
 
+new_string = ""
+x = str.length
+while x > 0 
+    x -= 1
+    new_string += str[x]
+end
+
+return new_string
 
 end
--- lib/reverse_words.rb
+++ lib/reverse_words.rb
@@ -6,5 +6,13 @@ def reverse_words(str)
 #Take the last element in arr and place in new_arr
 #Continue the process adding to the end of new_arr
 
+arr = str.split(" ")
+new_arr = []
+x = arr.length
+while x > 0
+x-= 1
+new_arr << arr[x]
+end
+return new_arr.join(" ")
 
 end
