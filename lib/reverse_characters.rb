bc40b8b3f25540728f7f64f659cda3b36096f1d9
Nels Findley <ncfindley2@gmail.com>
2016-06-13 23:24:49 +0000

    Nels Findley

--- lib/reverse_characters.rb
+++ lib/reverse_characters.rb
@@ -1,2 +1,19 @@
 def reverse_characters(str)
+    
+    x = 0
+    y = str.length
+    newstr = ""
+    
+    while  x < str.length
+        
+        x += 1
+        y -= 1
+        
+        
+        newstr << str[y]
+    
+    end
+    
+    return newstr
+    
 end
