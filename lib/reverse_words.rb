165eeb0ed3c68eaf4625bf787ef1b4eab8bac792
Nikki <nicleemart@gmail.com>
2016-06-14 23:28:26 +0000

    Completed Ruby reverse string

--- lib/reverse_words.rb
+++ lib/reverse_words.rb
@@ -1,2 +1,12 @@
+# For each individual word in the string, split at space. Add each word to new string in inverse order.
+
 def reverse_words(str)
+	words = str.split(" ")
+	new_sentence = []
+	index = words.length
+	while index > 0
+		index -= 1
+		new_sentence << words[index]
+	end
+	new_sentence.join(" ")
 end
