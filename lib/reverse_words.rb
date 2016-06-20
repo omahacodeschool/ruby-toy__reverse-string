c681cc9b23a0f908e891777f7dc7458fbb64bddc
Jerry Samuels Jr <jsamuelsjr@gmail.com>
2016-06-20 19:12:13 +0000

    Most current submission to correct indentation in both and array creation in the reverse words.

--- lib/reverse_words.rb
+++ lib/reverse_words.rb
@@ -1,12 +1,13 @@
 #To test my code in repl.it, created an str variable using the line below:
 #str = "Today is a good day".split(" ")
 def reverse_words(str)
-	arr_end = str.length - 1
+	str2 = str.split(" ")
+	arr_end = str2.length - 1
 	new_arr = Array.new
 	
 	(arr_end).downto(0) do |i|
 		#Create a variable to hold the array element's value for each iteration 
-		z = str[i]
+		z = str2[i]
 		#Add each iteration's value to the end of the array
 		new_arr.push(z)
 	end
@@ -15,6 +16,6 @@ def reverse_words(str)
 	fnl_arr = new_arr.join(" ")
 	
 	#Return value of final variable
-	puts fnl_arr
+	return fnl_arr
     
 end
