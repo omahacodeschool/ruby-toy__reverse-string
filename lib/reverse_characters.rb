656c62e8b62c47cfc8be1ca6a3461356891084eb
Jerry Samuels Jr <jsamuelsjr@gmail.com>
2016-06-20 19:02:07 +0000

    Resubmitting to correct indentation.

--- lib/reverse_characters.rb
+++ lib/reverse_characters.rb
@@ -2,20 +2,19 @@
 #str = "Today is a good day".split("")
 
 def reverse_characters(str)
-arr_end = str.length - 1
-new_arr = Array.new
-
-(arr_end).downto(0) do |i|
-	#Create a variable to hold the array element's value for each iteration 
-	z = str[i]
-	#Add each iteration's value to the end of the array
-	new_arr.push(z)
-end
-
-#Creates the final reversed string
-fnl_arr = new_arr.join
-
-#Print value of final variable
-puts fnl_arr
-    
+	arr_end = str.length - 1
+	new_arr = Array.new
+	
+	(arr_end).downto(0) do |i|
+		#Create a variable to hold the array element's value for each iteration 
+		z = str[i]
+		#Add each iteration's value to the end of the array
+		new_arr.push(z)
+	end
+	
+	#Creates the final reversed string
+	fnl_arr = new_arr.join
+	
+	#Return value of final variable
+	return fnl_arr
 end
--- lib/reverse_words.rb
+++ lib/reverse_words.rb
@@ -1,20 +1,20 @@
 #To test my code in repl.it, created an str variable using the line below:
 #str = "Today is a good day".split(" ")
 def reverse_words(str)
-arr_end = str.length - 1
-new_arr = Array.new
-
-(arr_end).downto(0) do |i|
-	#Create a variable to hold the array element's value for each iteration 
-	z = str[i]
-	#Add each iteration's value to the end of the array
-	new_arr.push(z)
-end
-
-#Creates the final reversed string, and separates each element with a space delimiter when joining the string together
-fnl_arr = new_arr.join(" ")
-
-#Print value of final variable
-puts fnl_arr
+	arr_end = str.length - 1
+	new_arr = Array.new
+	
+	(arr_end).downto(0) do |i|
+		#Create a variable to hold the array element's value for each iteration 
+		z = str[i]
+		#Add each iteration's value to the end of the array
+		new_arr.push(z)
+	end
+	
+	#Creates the final reversed string, and separates each element with a space delimiter when joining the string together
+	fnl_arr = new_arr.join(" ")
+	
+	#Return value of final variable
+	puts fnl_arr
     
 end
